b609a212540074d6d4617dab9b6537361a4c0d37
brian1803 <134600@students.nccvt.k12.de.us>
2020-12-16 20:50:24 +0000

    task4

--- prompt.js
+++ prompt.js
@@ -1,2 +1,20 @@
+/*task 1 */
 const one = prompt("pick a number please")
 const two = prompt("pick a another number please")
+
+/*task 2 */
+const one1 = parseInt(one);
+const two2 = parseInt(two);
+
+/*task 3*/
+console.log(one1 - one2);
+console.log(one1 + one2);
+console.log(one1 / one2);
+console.log(one1 * one2);
+console.log(one1 ** one2);
+console.log(one1 % one2);
+
+/* task 4 */
+
+const random = Math.floor(Math.random() * 35) + 1;
+alert("The random number is ${random}");
